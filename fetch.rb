70f7736cca61fb6e8a3fec41e14c59043b7a1bf5
Chad Barlow <chadlouis07@gmail.com>
2025-08-27 16:11:56 -0600

    adjusted fetch.rb script

--- fetch.rb
+++ fetch.rb
@@ -2,7 +2,6 @@ require "json"
 require "uri"
 require "net/http"
 
-# Example: fetch details about the Ruby repo from GitHub's API
 url = "https://api.github.com/repos/ruby/ruby"
 uri = URI(url)
 
@@ -11,6 +10,8 @@ abort("HTTP #{res.code}") unless res.is_a?(Net::HTTPSuccess)
 
 data = JSON.parse(res.body)
 
-puts "Repo: #{data["full_name"]}"
-puts "Stars: #{data["stargazers_count"]}"
-puts "Forks: #{data["forks_count"]}"
+#puts data["key"]
+#puts data["key2"]
+#puts data["key3"]
+
+puts JSON.pretty_generate(data)
